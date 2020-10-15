0ac9b0de50c4e5c937c4e4d4876bdf2daa3add64
mgcalvette <71100280+mgcalvette@users.noreply.github.com>
2020-10-15 19:17:43 -0300

    retoque
    -

--- Parcial 1/src/informes.h
+++ Parcial 1/src/informes.h
@@ -7,11 +7,36 @@
 
 #ifndef INFORMES_H_
 #define INFORMES_H_
-
+/** \brief  Imprime clientes y avisos
+ *\param Cliente puntero al array clientes
+ *\param int limite del array
+ *\param int limite del array
+ * \return int Return devuelve en 0 si esta todo bien y -1 si algun parametro es incorrecto
+ */
 int inf_impClientesYAvisos(Cliente*pBufferCli,int limiteCli,Publicacion* pBufferPubli,int limitePubli);
+/** \brief  Imprime cliente con mas avisos
+  *\param Publicacion puntero al array publicaciones
+ *\param int limite del array
+ *\param Cliente puntero al array clientes
+ *\param int limite del array
+ * \return int Return devuelve en 0 si esta todo bien y -1 si algun parametro es incorrecto
+ */
 int inf_clienteMasAvisos(Publicacion* pBufferPubli,int limitePubli,Cliente* pBufferCli,int limiteCli);
+/** \brief  Imprime cliente con mas avisos pausados
+  *\param Publicacion puntero al array publicaciones
+ *\param int limite del array
+ *\param Cliente puntero al array clientes
+ *\param int limite del array
+ * \return int Return devuelve en 0 si esta todo bien y -1 si algun parametro es incorrecto
+ */
 int inf_clienteMasAvisosPausados(Publicacion* pBufferPubli,int limitePubli,Cliente* pBufferCli,int limiteCli);
-
+/** \brief  Imprime cliente con mas avisos
+  *\param Publicacion puntero al array publicaciones
+ *\param int limite del array
+ *\param Cliente puntero al array clientes
+ *\param int limite del array
+ * \return int Return devuelve en 0 si esta todo bien
+ */
 int inf_rubroMasAvisos(Publicacion* pBufferPubli,int limitePubli, Cliente* pBufferCli, int limiteCli);
 
 #endif /* INFORMES_H_ */
--- Parcial 1/src/publicacion.h
+++ Parcial 1/src/publicacion.h
@@ -19,14 +19,27 @@ typedef struct{
     int isEmpty;
 }Publicacion;
 
-int publi_alta(Publicacion* pBuffer,int idCliente,int limite,char* nombreArchivo, int numeroRubro);
+
+/** \brief  devuelve un numero de Id que no se repite.
+ * \return int Return comienza en 0 y se incrementa en 1
+ */
 int publi_obtenerID();
+/** \brief  Da de alta un nuevo aviso.
+ *\param Publicacion puntero al array publicaciones
+ *\param int id del cliente
+ *\param int limite del array
+ *\param char nombre del archivo a cargar (64 caracteres)
+ *\param int numero de rubro a definir
+ * \return int Return devuelve en 0 si esta todo bien y -1 si algun parametro es incorrecto
+ */
+int publi_alta(Publicacion* pBuffer,int idCliente,int limite,char* nombreArchivo, int numeroRubro);
+
 int publi_inicializarArray(Publicacion* pBuffer,int limite);
+
 /*Probandola*/
 int publi_ImprimiByIdCliente(Publicacion* pBufferPubli,int limitePubli,Cliente* pBufferCli,int limiteCli,int id,int* indice);
 
 int publi_existeId(Publicacion* pBuffer,int limite,int id);
-
 
 int publi_contadorActivas(Publicacion* pBuffer,int limite,int idCliente);
 
